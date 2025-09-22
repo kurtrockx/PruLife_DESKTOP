2d4f17091784dd3e335af3295b363984253807a2
derpfear <kurtdebelen431@gmail.com>
2025-09-22 23:03:41 +0800

    design layout of table and header on the Clients page

--- src/pages/ClientsPage.jsx
+++ src/pages/ClientsPage.jsx
@@ -22,14 +22,103 @@ const CLIENTS = [
     status: "Inactive",
     actions: "Edit/Delete",
   },
+  {
+    name: "John Doe",
+    age: 35,
+    policyType: "Life Insurance",
+    status: "Active",
+    actions: "Edit/Delete",
+  },
+  {
+    name: "Jane Smith",
+    age: 28,
+    policyType: "Health Insurance",
+    status: "Pending",
+    actions: "Edit/Delete",
+  },
+  {
+    name: "Robert Brown",
+    age: 42,
+    policyType: "Car Insurance",
+    status: "Inactive",
+    actions: "Edit/Delete",
+  },
+  {
+    name: "John Doe",
+    age: 35,
+    policyType: "Life Insurance",
+    status: "Active",
+    actions: "Edit/Delete",
+  },
+  {
+    name: "Jane Smith",
+    age: 28,
+    policyType: "Health Insurance",
+    status: "Pending",
+    actions: "Edit/Delete",
+  },
+  {
+    name: "Robert Brown",
+    age: 42,
+    policyType: "Car Insurance",
+    status: "Inactive",
+    actions: "Edit/Delete",
+  },
+  {
+    name: "John Doe",
+    age: 35,
+    policyType: "Life Insurance",
+    status: "Active",
+    actions: "Edit/Delete",
+  },
+  {
+    name: "Jane Smith",
+    age: 28,
+    policyType: "Health Insurance",
+    status: "Pending",
+    actions: "Edit/Delete",
+  },
+  {
+    name: "Robert Brown",
+    age: 42,
+    policyType: "Car Insurance",
+    status: "Inactive",
+    actions: "Edit/Delete",
+  },
+  {
+    name: "John Doe",
+    age: 35,
+    policyType: "Life Insurance",
+    status: "Active",
+    actions: "Edit/Delete",
+  },
+  {
+    name: "Jane Smith",
+    age: 28,
+    policyType: "Health Insurance",
+    status: "Pending",
+    actions: "Edit/Delete",
+  },
+  {
+    name: "Robert Brown",
+    age: 42,
+    policyType: "Car Insurance",
+    status: "Inactive",
+    actions: "Edit/Delete",
+  },
 ];
 
 export default function ClientPage() {
   return (
-    <div className="flex flex-1 items-center justify-center">
-      <div className="flex max-w-[70dvw] flex-1 flex-col">
-        <Categories />
-        <ClientsList />
+    <div className="flex flex-1 flex-col justify-center">
+      <h1 className="mx-auto pt-20 text-2xl font-semibold">
+        CLIENT MANAGEMENT PAGE
+      </h1>
+      <div className="flex flex-1 items-center justify-center">
+        <div className="flex max-h-[60dvh] max-w-[70dvw] flex-1 flex-col overflow-y-scroll rounded-tl-xl rounded-bl-xl border">
+          <Categories />
+          <ClientsList />
+        </div>
       </div>
     </div>
   );
@@ -37,10 +126,10 @@ export default function ClientPage() {
 
 function Categories() {
   return (
-    <div className="flex flex-1 gap-10 border px-4">
+    <div className="sticky top-0 flex flex-1 border border-[#450509] bg-[#450509] px-4 font-semibold text-white shadow-sm">
       {CATEGORIES.map((c, i) => (
         <h2
-          className={`flex-1 ${i === CATEGORIES.length - 1 && "border-transparent"} border-r px-2 text-center text-xl whitespace-nowrap uppercase`}
+          className={`flex-1 py-2 ${i === CATEGORIES.length - 1 && "border-transparent"} mx-auto border-r border-black px-2 text-center text-xl whitespace-nowrap uppercase`}
         >
           {c}
         </h2>
@@ -51,7 +140,7 @@ function Categories() {
 
 function ClientsList() {
   return (
-    <div className="flex flex-col border p-4 px-2">
+    <div className="flex flex-col gap-y-2 p-4 px-2">
       {CLIENTS.map((c) => {
         const { name, age, policyType, status, actions } = c;
         const clientDetails = [name, age, policyType, status, actions];
@@ -64,10 +153,10 @@ function ClientsList() {
 
 function Client({ client }) {
   return (
-    <div className="flex flex-1 gap-10 border px-2">
+    <div className="flex flex-1 border px-2">
       {client.map((c, i) => (
         <h3
-          className={`flex-1 border-r ${i === client.length - 1 && "border-transparent"} px-2 text-xl whitespace-nowrap`}
+          className={`flex-1 border-r py-2 ${i === client.length - 1 && "border-transparent"} px-2 text-xl whitespace-nowrap`}
         >
           {c}
         </h3>
