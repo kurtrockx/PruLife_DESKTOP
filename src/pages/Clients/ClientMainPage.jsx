f76c8ef9ca53ad6153efcefcf755dc5b0ecf4a6f
derpfear <kurtdebelen431@gmail.com>
2025-10-22 05:43:50 +0800

    before pulling from laptop for validation

--- src/pages/Clients/ClientMainPage.jsx
+++ src/pages/Clients/ClientMainPage.jsx
@@ -2,7 +2,7 @@ import { NavLink, Outlet, useLocation } from "react-router-dom";
 
 export default function ClientMainPage() {
   const ClientTabStyle =
-    "rounded-xl p-2 shadow-sm duration-150 hover:-translate-y-0.5";
+    "rounded-xl p-2 shadow-sm duration-150 hover:-translate-y-0.5 dark:bg-black dark:text-white";
 
   const location = useLocation();
   const currentTabOpen = location.pathname.split("/")[3];
@@ -18,7 +18,7 @@ export default function ClientMainPage() {
         </NavLink>
       </div>
       <div
-        className={`flex aspect-video max-h-[90%] flex-1 overflow-hidden rounded-2xl dark:border-white/40 dark:border bg-white shadow-[0_0_10px] shadow-black/20 max-2xl:max-w-[90%] dark:bg-black ${currentTabOpen === "chat" && "flex-col"}`}
+        className={`flex aspect-video max-h-[90%] flex-1 overflow-hidden rounded-2xl bg-white shadow-[0_0_10px] shadow-black/20 max-2xl:max-w-[90%] dark:border dark:border-white/40 dark:bg-black ${currentTabOpen === "chat" && "flex-col"}`}
       >
         <Outlet />
       </div>
